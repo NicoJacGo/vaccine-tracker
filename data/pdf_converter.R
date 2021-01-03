c28a472b07e9c1ca058b8e302c0580ee73ed7c75
NicoJacGo <50533289+NicoJacGo@users.noreply.github.com>
2021-01-02 18:28:20 -0600

    pdf converter

---
+++ data/pdf_converter.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(rvest)
+library(pdftools)
+
+url_modifier <- function(num) {
+  sprintf(paste0("https://www.dshs.texas.gov/news/updates/COVIDVaccineAllocation-Week", num, ".pdf"))
+}
+
+week <- 0
+
+for(i in seq(1, 1, 1)){
+  week <<- week + i #assigning to global environment 
+}
+
+url_modifier(week)
+
+download.file(url_modifier(week), basename(url_modifier(week)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
