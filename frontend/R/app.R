52eb3145e4d3a76f85ba9ed54a7c91b83d0f79d6
hanadhassan <hanadhassan2000@gmail.com>
2021-01-03 22:47:13 -0500

    frontend start

---
+++ frontend/R/app.R
@@ -0,0 +1,51 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+library(shinythemes)
+
+
+# Define UI
+ui <- fluidPage(theme = shinytheme("cerulean"),
+                navbarPage(
+                    # theme = "cerulean",  # <--- To use a theme, uncomment this
+                    "My first app",
+                    tabPanel("Navbar 1",
+                             sidebarPanel(
+                                 tags$h3("Input:"),
+                                 textInput("txt1", "Given Name:", ""),
+                                 textInput("txt2", "Surname:", ""),
+                                 
+                             ), # sidebarPanel
+                             mainPanel(
+                                 h1("Header 1"),
+                                 
+                                 h4("Output 1"),
+                                 verbatimTextOutput("txtout"),
+                                 
+                             ) # mainPanel
+                             
+                    ), # Navbar 1, tabPanel
+                    tabPanel("Navbar 2", "This panel is intentionally left blank"),
+                    tabPanel("Navbar 3", "This panel is intentionally left blank")
+                    
+                ) # navbarPage
+) # fluidPage
+
+
+# Define server function  
+server <- function(input, output) {
+    
+    output$txtout <- renderText({
+        paste( input$txt1, input$txt2, sep = " " )
+    })
+} # server
+
+
+# Create Shiny object
+shinyApp(ui = ui, server = server)
