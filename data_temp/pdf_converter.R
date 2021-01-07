be6c7900332d1b98dbeba8bf54243d90ca575c81
NicoJacGo <50533289+NicoJacGo@users.noreply.github.com>
2021-01-06 19:31:04 -0600

    pdf conversion

--- data_temp/pdf_converter.R
+++ data_temp/pdf_converter.R
@@ -6,7 +6,7 @@ url_modifier <- function(num) {
   sprintf(paste0("https://www.dshs.texas.gov/news/updates/COVIDVaccineAllocation-Week", num, ".pdf"))
 }
 
-week <- 0
+week <- 3
 
 for(i in seq(1, 1, 1)){
   week <<- week + i #assigning to global environment 
--- data_temp/pdf_to_csv.r
+++ data_temp/pdf_to_csv.r
@@ -1,6 +1,13 @@
 library(pdftables)
 
-write.csv(head(iris, 20), file = "COVIDVaccineAllocation-Week1.csv", row.names = FALSE)
+# pdf tables api key only valid for 50 pages
 
-convert_pdf("COVIDVaccineAllocation-Week1.pdf", "COVIDVaccineAllocation-Week1.csv")
+text <- pdf_text("COVIDVaccineAllocation-Week4.pdf")
 
+convert_pdf("COVIDVaccineAllocation-Week4.pdf", output_file = "week4.csv", format="csv",message = TRUE, api_key = "sldcc9e4wnhk")
+
+# header tailoring
+
+df <- read.csv("week4.csv")
+names(df) <- as.character(df[1,])
+df <- df[2:nrow(df),]
