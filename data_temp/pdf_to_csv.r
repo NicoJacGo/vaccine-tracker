d1f0298ce8e4f5fdef79a999acd70ef7c8e25d2b
NicoJacGo <50533289+NicoJacGo@users.noreply.github.com>
2021-01-12 21:59:39 -0500

    data updates

--- data_temp/pdf_to_csv.r
+++ data_temp/pdf_to_csv.r
@@ -1,13 +1,14 @@
 library(pdftables)
+library(pdftools)
 
 # pdf tables api key only valid for 50 pages
 
-text <- pdf_text("COVIDVaccineAllocation-Week4.pdf")
+text <- pdf_text("COVIDVaccineAllocation-Week5.pdf")
 
-convert_pdf("COVIDVaccineAllocation-Week4.pdf", output_file = "week4.csv", format="csv",message = TRUE, api_key = "sldcc9e4wnhk")
+convert_pdf("COVIDVaccineAllocation-Week5.pdf", output_file = "week4.csv", format="csv",message = TRUE, api_key = "sldcc9e4wnhk")
 
 # header tailoring
 
-df <- read.csv("week4.csv")
+df <- read.csv("week5.csv")
 names(df) <- as.character(df[1,])
 df <- df[2:nrow(df),]
