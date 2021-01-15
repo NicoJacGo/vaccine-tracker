19eaaa4dae62efe4a85af9f22751e8f7beca2a42
hanadhassan <hanadhassan2000@gmail.com>
2021-01-15 13:26:33 -0500

    improved map UI

--- app_temp.R
+++ app_temp.R
@@ -123,11 +123,35 @@ library(maps)
   
   # Define server function  
   server <- function(input, output) {
+    tx_data <- read_csv("new_aggregate.csv")
+    #data.frame(test2)
+    tx_data <- data.frame(tx_data)
+    colnames(tx_data)
+    colnames(tx_data)[colnames(tx_data) == 'lat'] <- 'Latitude'
+    colnames(tx_data)[colnames(tx_data) == 'lon'] <- 'Longitude' 
+    colnames(tx_data)
+    tx_data$Latitude <- as.numeric(tx_data$Latitude)
+    tx_data$Longitude <- as.numeric(tx_data$Longitude)
+    tx_data = filter(tx_data, Latitude != "NA")
+    pal <- colorFactor(pal = c("#1b9e77", "#d95f02", "#7570b3"), domain = c("Charity", "Government", "Private"))
+    tx_data <- mutate(tx_data, cntnt=paste0('<strong>Provider Name: </strong>',Provider_Name,
+                                            '<br><strong>Address:</strong> ', Address,
+                                            '<br><strong>City:</strong> ', City,
+                                            '<br><strong>County:</strong> ',County,
+                                            '<br><strong>Doses (Week 5):</strong> ',Doses_W5,
+                                            '<br><strong>Total:</strong> ',Sum)) 
     output$mymap <- renderLeaflet({
-      tx <- tigris::counties(state = "TX", cb=T)
-      leaflet(tx) %>%
+      leaflet(tx_data) %>% 
+        addCircles(lng = ~Longitude, lat = ~Latitude) %>% 
         addTiles() %>%
-        addPolygons(color = "grey", fillOpacity = 0.1, smoothFactor = 0.5, weight = 2)
+        addCircleMarkers(data = tx_data, lat =  ~Latitude, lng =~Longitude, 
+                         radius = 3, popup = ~as.character(cntnt), 
+                         #color = ~pal(Category),
+                         stroke = FALSE, fillOpacity = 0.8)%>%
+        addLegend(pal=pal, values=tx_data$SumDosesRecd,opacity=1, na.label = "Not Available")%>%
+        addEasyButton(easyButton(
+          icon="fa-crosshairs", title="ME",
+          onClick=JS("function(btn, map){ map.locate({setView: true}); }")))
     })
     output$txtout <- renderText({
       paste( input$txt1, input$txt2, sep = " " )
