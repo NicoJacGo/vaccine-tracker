505749ee669fe6a2e847b54e4284005f050b28e5
NicoJacGo <50533289+NicoJacGo@users.noreply.github.com>
2021-01-15 23:34:22 -0500

    modifications to file

--- app.R
+++ app.R
@@ -1,128 +1,157 @@
-## COVID-2019 interactive vaccine tracker tool
-## Nicolas Jacobs-Goraya & Hanad Hassan
-
-# https://rviews.rstudio.com/2019/10/09/building-interactive-world-maps-in-shiny/
-
-# update data with automated script
-source("texas_data_weekly_vaccinations.R") # run locally to update numbers, but not live on Rstudio server /Users/epp11/Dropbox (VERG)/GitHub/nCoV_tracker/app.R(to avoid possible errors on auto-updates)
-
-# load required packages
-if(!require(magrittr)) install.packages("magrittr", repos = "http://cran.us.r-project.org")
-if(!require(rvest)) install.packages("rvest", repos = "http://cran.us.r-project.org")
-if(!require(readxl)) install.packages("readxl", repos = "http://cran.us.r-project.org")
-if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
-if(!require(maps)) install.packages("maps", repos = "http://cran.us.r-project.org")
-if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
-if(!require(reshape2)) install.packages("reshape2", repos = "http://cran.us.r-project.org")
-if(!require(ggiraph)) install.packages("ggiraph", repos = "http://cran.us.r-project.org")
-if(!require(RColorBrewer)) install.packages("RColorBrewer", repos = "http://cran.us.r-project.org")
-if(!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-project.org")
-if(!require(plotly)) install.packages("plotly", repos = "http://cran.us.r-project.org")
-if(!require(geojsonio)) install.packages("geojsonio", repos = "http://cran.us.r-project.org")
-if(!require(shiny)) install.packages("shiny", repos = "http://cran.us.r-project.org")
+# Load R packages
+library(tidyverse)
+library(shiny)
+library(shinythemes)
 if(!require(shinyWidgets)) install.packages("shinyWidgets", repos = "http://cran.us.r-project.org")
 if(!require(shinydashboard)) install.packages("shinydashboard", repos = "http://cran.us.r-project.org")
-if(!require(shinythemes)) install.packages("shinythemes", repos = "http://cran.us.r-project.org")
-if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-if(!require(ggmap)) install.packages("ggmap", repos = "http://cran.us.r-project.org")
-if(!require(sf)) install.packages("sf", repos = "http://cran.us.r-project.org")
-if(!require(mapview)) install.packages("mapview", repos = "http://cran.us.r-project.org")
-
-# set mapping colour for each vaccine manufacturer
-pfizer_col = "#cc4c02"
-moderna_col = "#662506"
-both_col = "#045a8d"
-
-# imported data
-# counties = 
-# vaccine_data = 
-# populations = 
-
-### MAP FUNCTIONS ###
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# testing  
-register_google(key="AIzaSyCypbYJVA5Yeht5FI92lq2UUGZHxfdZu84")
-
-Texas = get_map(location = c(lon = -100, lat = 31), zoom = 6, maptype = "terrain")
-ggmapplot(Texas)
-
-test <- read_csv("C:/Users/Hanad/Documents/vaccine-tracker/vaccine-tracker/input_data/test_data.csv")
-
-sources <- distinct(test, Address, County)
-
-df <- as.data.frame(sources)
-
-locations_df <- mutate_geocode(df, Address)
-
-locations <- as_tibble(locations_df)
-
-locations_sf <- st_as_sf(locations, coords = c("lon", "lat"), crs = 4326)
-
-mapview(locations_sf)
-
-<<<<<<< HEAD
-#get("https://og-production-open-data-dentontx-892364687672.s3.amazonaws.com/resources/be1b384f-3b2e-4238-88a8-4410cb36f7fc/c8efcc13-b5b4-48b5-9873-933844822d6ctexascounties.geojson?Signature=Gae8hChkUibDO5r1Erz2vN3EE%2BA%3D&Expires=1609901223&AWSAccessKeyId=AKIAJJIENTAPKHZMIPXQ")
-
-=======
->>>>>>> 65d53a674381cb06ade6e8997a59c1b4bacdffd2
-
-mapview(list(franconia, locations_sf),
-        layer.name = c("Texas", "Locations"))
-
-library(lattice)
+if(!require(RColorBrewer)) install.packages("RColorBrewer", repos = "http://cran.us.r-project.org")
+if(!require(leaflet)) install.packages("leaflet", repos = "http://cran.us.r-project.org")
+if(!require(maps)) install.packages("maps", repos = "http://cran.us.r-project.org")
+if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+library(tigris)
+library(sf)
+library(raster)
+library(rgeos)
+library(mapview)
+library(maptools)
 library(sp)
+library(maps)
+library(dplyr)
+library(DT)
+library(leaflet.extras)
 
-data(meuse)
+# data imports
 
+tx_data_g = read_csv("new_aggregate.csv")
 
-tx_data <- read_csv("C:/Users/Hanad/Documents/vaccine-tracker/vaccine-tracker/data_temp/data.csv")
-#data.frame(test2)
-tx_data <- data.frame(tx_data)
-colnames(tx_data)
-colnames(tx_data)[colnames(tx_data) == 'Latitude..generated.'] <- 'Latitude'
-colnames(tx_data)[colnames(tx_data) == 'Longitude..generated.'] <- 'Longitude' 
-colnames(tx_data)
-tx_data$Latitude <- as.numeric(tx_data$Latitude)
-tx_data$Longitude <- as.numeric(tx_data$Longitude)
-pal <- colorFactor(pal = c("#1b9e77", "#d95f02", "#7570b3"), domain = c("Charity", "Government", "Private"))
-
-library(leaflet)
-bbmap <- renderLeaflet({
-  leaflet(tx_data) %>% 
-    addCircles(lng = ~Longitude, lat = ~Latitude) %>% 
-    addTiles() %>%
-    addCircleMarkers(data = bb_data, lat =  ~Latitude, lng =~Longitude, 
-                     #radius = 3, popup = ~as.character(cntnt), 
-                     color = ~pal(Category),
-                     stroke = FALSE, fillOpacity = 0.8)%>%
-    addLegend(pal=pal, values=tx_data$SumDosesRecd,opacity=1, na.label = "Not Available")%>%
-    addEasyButton(easyButton(
-      icon="fa-crosshairs", title="ME",
-      onClick=JS("function(btn, map){ map.locate({setView: true}); }")))
-})
-
-bbmap
-
-
-tx_data %>%
-  leaflet() %>%
-  addTiles %>%
-  addCircleMarkers()
+  ui <- bootstrapPage(
+    navbarPage(theme = shinytheme("flatly"), collapsible = TRUE,
+               HTML('<a style="text-decoration:none;cursor:default;color:#FFFFFF;" class="active" href="#">Texas Vaccine Tracker</a>'), id="nav",
+               windowTitle = "COVID-19 Vaccine Tracker",
+               tabPanel("Map",
+                        div(class="outer",
+                            tags$head(includeCSS("styles.css")),
+                            leafletOutput("mymap", width="100%", height="100%"),
+                            
+                            absolutePanel(id = "logo", class = "card", bottom = 20, left = 20, width = 30, fixed=TRUE, draggable = FALSE, height = "auto",
+                                          actionButton("twitter_share", label = "", icon = icon("twitter"),style='padding:5px',
+                                                       onclick = sprintf("window.open('%s')", 
+                                                                         "https://twitter.com/intent/tweet?text=%20Texas-Vaccine-Tracker%20mapper&url=https://bit.ly/2uBvnds&hashtags=coronavirus")))
+                            
+                            
+                        )
+               ),
+               navbarMenu(title = "County Analysis",
+                          tags$a("UNDER CONSTRUCTION"),
+                          tabPanel(title = a("Bins")), tabPanel(title = a("Trends")), tabPanel(title = a("Projections"))
+                        
+                        
+                        
+                        ),
+               
+               tabPanel("Data",
+                        numericInput("maxrows", "Rows to show", 25),
+                        verbatimTextOutput("rawtable"),
+                        downloadButton("downloadCsv", "Download as CSV"),tags$br(),tags$br(),
+                        "Adapted from data published by the", tags$a(href="https://www.dshs.texas.gov/coronavirus/immunize/vaccine.aspx", 
+                                                                  "Texas Health and Human Services.")
+               ),
+               tabPanel("About",
+                        tags$div(
+                          tags$h4("Last update"), 
+                          #h6(paste0(update)),
+                          "This site is updated once a week. There are several other excellent vaccine tracking tools available, including those run by", 
+                          tags$a(href="https://www.bloomberg.com/graphics/covid-vaccine-tracker-global-distribution/", "Bloomberg"),
+                          
+                          "Our aim is to complement these resources with several interactive features as well as visualization of data detailing pharmacy locations vaccine dosage allotments.",
+                          
+                          tags$br(),tags$br(),tags$h4("Background"), 
+                          "We intend on making this web application available to the general populace, giving them the ability to easily locate the nearest location offering vaccines, with the given weekly vaccine allotment and previous weekly data, along with aggregate weekly allotment data.
+                          This dataset was created to give users the ability to examine Texas' COVID vaccine allotment progress, which is an ongoing process. The data will be updated on a weekly basis, and aggregated in the aggregate.csv file. New data is provided once weekly, on Tuesday, by the Texas DSHS and converted from PDF to .csv",
+                          tags$br(),tags$br(),
+                          "This dataset was created to give users the ability to examine Texas' COVID vaccine allotment progress, which is an ongoing process. The data will be updated on a weekly basis, and aggregated in the *aggregate.csv* file. Data is aggregated from data published by the Texas DSHS. 
+                          The raw data, published in .pdf format, is available from the raw folder.",
+                          tags$br(),tags$br(),
+                          tags$br(),tags$br(),tags$h4("Sources"),
+                          tags$b("Texas vaccination statistics"), tags$a(href="https://tabexternal.dshs.texas.gov/t/THD/views/COVID-19VaccineinTexasDashboard/Summary?%3Aorigin=card_share_link&%3Aembed=y&%3AisGuestRedirectFromVizportal=y", "the Texas DSHS homepage"),
+                          tags$b("weekly vaccine allocation reports: "), tags$a(href="https://www.dshs.texas.gov/coronavirus/immunize/vaccine.aspx", "the Texas DSHS"),
+                          tags$br(),tags$br(),tags$h4("Authors"),
+                          "Nicolas Jacobs, University of Toronto",tags$br(),
+                          "Hanad Hassan, University of Toronto",tags$br(),
+                          tags$br(),tags$br(),tags$h4("Contact"),
+                          tags$a(href="https://www.nicojacgo.com", "Nicolas Jacobs"),tags$br(),tags$br(),
+                          tags$a(href="https://www.hanadhassan.com", "Hanad Hassan"),tags$br(),tags$br()
+                    
+                        )
+               )
+           )
+  )
   
-
-
   
+  
+  # Define server function  
+  server <- function(input, output) {
+    tx_data <- read_csv("new_aggregate.csv")
+    #data.frame(test2)
+    tx_data <- data.frame(tx_data)
+    colnames(tx_data)
+    colnames(tx_data)[colnames(tx_data) == 'lat'] <- 'Latitude'
+    colnames(tx_data)[colnames(tx_data) == 'lon'] <- 'Longitude' 
+    colnames(tx_data)
+    tx_data$Latitude <- as.numeric(tx_data$Latitude)
+    tx_data$Longitude <- as.numeric(tx_data$Longitude)
+    tx_data = filter(tx_data, Latitude != "NA")
+    pal <- colorFactor(pal = c("#1b9e77", "#d95f02", "#7570b3"), domain = c("Charity", "Government", "Private"))
+    tx_data <- mutate(tx_data, cntnt=paste0('<strong>Provider Name: </strong>',Provider_Name,
+                                            '<br><strong>Address:</strong> ', Address,
+                                            '<br><strong>City:</strong> ', City,
+                                            '<br><strong>County:</strong> ',County,
+                                            '<br><strong>Doses (Week 5):</strong> ',Doses_W5,
+                                            '<br><strong>Total:</strong> ',Sum)) 
+    output$mymap <- renderLeaflet({
+      leaflet(tx_data) %>%
+        leaflet.extras::addSearchOSM(options = searchOptions(collapsed = TRUE)) %>%
+        addTiles() %>%
+        addCircleMarkers(data = tx_data, lat =  ~Latitude, lng =~Longitude, 
+                         radius = 7.5, popup = ~as.character(cntnt), clusterOptions = markerClusterOptions(),
+                         color = "red",
+                         stroke = FALSE, fillOpacity = 0.8)%>%
+        addLegend(pal=pal, values=tx_data$SumDosesRecd,opacity=1, na.label = "Not Available")%>%
+        addEasyButton(easyButton(
+          icon="fa-crosshairs", title="ME",
+          onClick=JS("function(btn, map){ map.locate({setView: true}); }")))
+        
+      
+      
+    })
+    output$txtout <- renderText({
+      paste( input$txt1, input$txt2, sep = " " )
+    })
+    
+    output$downloadCsv <- downloadHandler(
+      filename = function() {
+        paste("COVID_Vaccine_Data_TX.csv", sep="")
+      },
+      content = function(file) {
+        tx_data_sub = tx_data %>% dplyr::select(c(Provider_Name, Address, City, County, Doses_W1, Doses_W2, Doses_W3,
+                                                  Doses_W4, Doses_W5, Sum))
+        names(tx_data_sub) = c("Provider", "Address", "City", "County", "Doses_W1","Doses_W2","Doses_W3", "Doses_W4", 
+                               "Doses_W5", "Total" )
+        write.csv(tx_data_sub, file)
+      }
+    )
+    
+    output$rawtable <- renderPrint({
+      tx_data_sub = tx_data %>% dplyr::select(c(Provider_Name, Address, City, County, Doses_W1, Doses_W2, Doses_W3,
+                                                Doses_W4, Doses_W5, Sum))
+      names(tx_data_sub) = c("Provider", "Address", "City", "County", "Doses_W1","Doses_W2","Doses_W3", "Doses_W4", 
+                             "Doses_W5", "Total" )
+      orig <- options(width = 1000)
+      print(tail(tx_data_sub, input$maxrows), row.names = FALSE)
+      options(orig)
+    })
+    
+  } # server
+  
+  # Create Shiny object
+  shinyApp(ui = ui, server = server)
