37f7c825b37e63d55c8f4205908e4841614fd892
NicoJacGo <50533289+NicoJacGo@users.noreply.github.com>
2021-02-02 22:08:42 -0500

    pushed new merge

--- data_temp/merge.r
+++ data_temp/merge.r
@@ -2,17 +2,24 @@ library(dplyr)
 library(readr)
 
 da <- read.csv("aggregate.csv")
+d6 <- read.csv("week6.csv")
 
-edf <- merge(d1,d2, by=intersect(names(d1), names(d2)), all = TRUE, all.d1= all, all.d2 = all)
+#edf <- merge(d1,d2, by=intersect(names(d1), names(d2)), all = TRUE, all.d1= all, all.d2 = all)
 
-edf <- merge(da,d5, by=intersect(names(da), names(d5)), all = TRUE, all.da= all, all.d5 = all)
+edf <- merge(da,d6, by=intersect(names(da), names(d6)), all = TRUE, all.da= all, all.d6 = all)
 
-da <- da[ , -which(names(da) %in% c("X.1"))]
+edf[11] <- NULL
 
-write.csv(da, file="aggregate.csv")
+edf <- edf[,c(1,2,3,4,5,6,7,8,9,11,10)]
 
-da <- da %>% 
+
+#da <- da[ , -which(names(da) %in% c("X"))]
+
+write.csv(edf, file="aggregate.csv")
+
+edf <- edf %>% 
   rowwise() %>% 
-  mutate("Sum" = sum(Doses_W1, Doses_W2, Doses_W3, Doses_W4, Doses_W5, na.rm = TRUE))
+  mutate("Sum" = sum(Doses_W1, Doses_W2, Doses_W3, Doses_W4, Doses_W5, Doses_W6, na.rm = TRUE))
 
 
+write.csv(edf, file="aggregate.csv")
